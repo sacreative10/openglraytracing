93a9790a2caffd4a16313765bbbdc895bb35188d
Sohaib Alam <sacreative10@gmail.com>
2022-06-06 19:44:37 +0100

    yay, it works

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,55 @@
 #include <string>
 #include <fstream>
 #include <cmath>
+#include <chrono>
+#include <vector>
+void NormalLogger(std::string message)
+{
+    auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string time_str = std::ctime(&time);
+    time_str.pop_back();
+    // print in yellow bold
+    std::cout << "\033[1;33m" << "[" << time_str << "]" << ": " << message << "\033[0m" << std::endl;
+}
+
+// Error logger
+// In red color outputs the message in bold
+// takes the message as a parameter
+// and the message is printed to the console, along with the time
+void ErrorLogger(std::string message)
+{
+    // time in hours, minutes, seconds
+    auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string time_string = std::ctime(&time);
+    time_string.pop_back();
+    std::cout << "\033[1;31m" << "[" << time_string << "]"<< ": " << message << "\033[0m" << std::endl;
+}
+
+void NormalLoggerFlush(std::string message)
+{
+    auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::string time_str = std::ctime(&time);
+    time_str.pop_back();
+    // print in yellow bold
+    std::cout << "\033[1;33m" << "[" << time_str << "]" << ": " << message << "\033[0m" << "\r";
+}
+
+void ForceTerminate()
+{
+    ErrorLogger("Forcing termination, this is a fatal error");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+}
 
 
 void error_callback(int error, const char* description)
 {
-    std::cerr << "Error: " << description << std::endl;
+    ErrorLogger(description);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    NormalLogger("Framebuffer size changed to " + std::to_string(width) + "x" + std::to_string(height));
     glViewport(0, 0, width, height);
 }
 
@@ -22,6 +62,7 @@ void input_callback(GLFWwindow* window)
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -59,7 +100,8 @@ GLuint FileToShader(const char* filename, GLenum shaderType)
     }
     else
     {
-        std::cerr << "Failed to open file: " << filename << std::endl;
+        ErrorLogger("Failed to open file: " + std::string(filename));
+        ForceTerminate();
         return 0;
     }
 
@@ -74,20 +116,21 @@ GLuint FileToShader(const char* filename, GLenum shaderType)
     if(!success)
     {
         glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cerr << "Failed to compile shader: " << filename << std::endl;
-        std::cerr << infoLog << std::endl;
+        // TODO: Only add infoLog if in DEBUG mode
+        ErrorLogger("Failed to compile shader: " + std::string(infoLog));
         glDeleteShader(shader);
+        ForceTerminate();
         return 0;
     }
-    std::cout << "Loaded shader: " << filename << std::endl;
+    NormalLogger("Compiled shader: " + std::string(filename));
     return shader;
 }
 
-GLuint createShaderProgram(const GLuint& vertexShader, const GLuint& fragmentShader)
+GLuint createShaderProgram(std::vector<GLuint> shaders)
 {
     GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
+    for(GLuint shader : shaders)
+        glAttachShader(shaderProgram, shader);
     glLinkProgram(shaderProgram);
 
     GLint success;
@@ -96,13 +139,19 @@ GLuint createShaderProgram(const GLuint& vertexShader, const GLuint& fragmentSha
     if(!success)
     {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cerr << "Failed to link shader program" << std::endl;
-        std::cerr << infoLog << std::endl;
+        // TODO: Only add infoLog if in DEBUG mode
+        ErrorLogger("Failed to link shader program: " + std::string(infoLog));
         glDeleteProgram(shaderProgram);
+        ForceTerminate();
         return 0;
     }
-    std::cout << "Created shader program" << std::endl;
+    NormalLogger("Linked shader program");
     return shaderProgram;
+}
+void DeleteShader(GLuint shader)
+{
+    NormalLogger("Cleaning up shader: " + std::to_string(shader));
+    glDeleteShader(shader);
 }
 
 
@@ -112,25 +161,36 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPENGL_MAJOR_VERSION);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPENGL_MINOR_VERSION);
     glfwWindowHint(GLFW_OPENGL_CORE_PROFILE, GL_TRUE);
-
+    NormalLogger("Initialising with OpenGL version: " + std::to_string(OPENGL_MAJOR_VERSION) + "." + std::to_string(OPENGL_MINOR_VERSION));
     glfwSetErrorCallback(error_callback);
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Raytracing In OpenGL", NULL, NULL);
     if(!window)
     {
-        glfwTerminate();
+        ErrorLogger("Failed to create window");
+        ForceTerminate();
         return EXIT_FAILURE;
     }
     glfwMakeContextCurrent(window);
-
+    // vsync
+    if(vSync){
+        glfwSwapInterval(1);
+        NormalLogger("Running with vsync");
+    }
+    else {
+        glfwSwapInterval(0);
+        NormalLogger("Running without vsync");
+    }
     // load glad
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        error_callback(0, "glad load failed");
+        ErrorLogger("Failed to initialize GLAD");
+        ForceTerminate();
         return EXIT_FAILURE;
     }
+    NormalLogger("Loaded glad");
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
     GLuint VAO, VBO, EBO;
     glCreateVertexArrays(1, &VAO);
@@ -160,36 +220,42 @@ int main()
     glTextureStorage2D(screenTex, 1, GL_RGBA32F, SCR_WIDTH, SCR_HEIGHT);
     glBindImageTexture(0, screenTex, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 
-    GLuint screenVertexShader = FileToShader("../src/shaders/ScreenVertexShader.vert.glsl", GL_VERTEX_SHADER);
-    GLuint screenFragmentShader = FileToShader("../src/shaders/ScreenFragmentShader.frag.glsl", GL_FRAGMENT_SHADER);
-    GLuint screenShaderProgram = createShaderProgram(screenVertexShader, screenFragmentShader);
+    GLuint screenVertexShader = FileToShader("../src/shaders/ScreenVertexShader.vert", GL_VERTEX_SHADER);
+    GLuint screenFragmentShader = FileToShader("../src/shaders/ScreenFragmentShader.frag", GL_FRAGMENT_SHADER);
+    GLuint screenShaderProgram = createShaderProgram({screenVertexShader, screenFragmentShader});
 
-    glDeleteShader(screenVertexShader);
-    glDeleteShader(screenFragmentShader);
+    DeleteShader(screenVertexShader);
+    DeleteShader(screenFragmentShader);
 
+    GLuint ComputeShader = FileToShader("../src/shaders/ComputeShader.comp", GL_COMPUTE_SHADER);
+    GLuint ComputeShaderProgram = createShaderProgram({ComputeShader});
 
-    GLuint ComputeShader = FileToShader("../src/shaders/ComputeShader.comp.glsl", GL_COMPUTE_SHADER);
-
-    GLuint ComputeShaderProgram = createShaderProgram(ComputeShader, (GLuint)0);
+    DeleteShader(ComputeShader);
 
     int workGroupCurrent[3];
     glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &workGroupCurrent[0]);
     glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 1, &workGroupCurrent[1]);
     glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 2, &workGroupCurrent[2]);
-    std::cout << "Max work group count per compute shader: " << workGroupCurrent[0] << " " << workGroupCurrent[1] << " " << workGroupCurrent[2] << std::endl;
+    NormalLogger("Max work group count: " + std::to_string(workGroupCurrent[0]) + " " + std::to_string(workGroupCurrent[1]) + " " + std::to_string(workGroupCurrent[2]));
 
     int workGroupSize[3];
     glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 0, &workGroupSize[0]);
     glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 1, &workGroupSize[1]);
     glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &workGroupSize[2]);
-    std::cout << "Max work group size: " << workGroupSize[0] << " " << workGroupSize[1] << " " << workGroupSize[2] << std::endl;
+    NormalLogger("Max work group size: " + std::to_string(workGroupSize[0]) + " " + std::to_string(workGroupSize[1]) + " " + std::to_string(workGroupSize[2]));
 
     int workGroupInv;
     glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &workGroupInv);
-    std::cout << "Max work group invocations: " << workGroupInv << std::endl;
+    NormalLogger("Max work group invocations: " + std::to_string(workGroupInv));
+
+    std::chrono::duration<double> meanFPS;
+    auto prevTime = std::chrono::high_resolution_clock::now();
+    int frameCount = 0;
 
     while(!glfwWindowShouldClose(window))
     {
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        frameCount++;
         input_callback(window);
         glUseProgram(ComputeShaderProgram);
         glDispatchCompute(ceil(SCR_WIDTH / 8), ceil(SCR_HEIGHT / 4), 1);
@@ -203,6 +269,13 @@ int main()
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+        if(frameCount % 100 == 0)
+        {
+            auto currTime = std::chrono::high_resolution_clock::now();
+            meanFPS = (currTime - prevTime) / 100;
+            prevTime = currTime;
+            NormalLogger("FPS: " + std::to_string(1 / meanFPS.count())); 
+        }
     }
 
     glDeleteVertexArrays(1, &VAO);
@@ -211,6 +284,7 @@ int main()
     glDeleteTextures(1, &screenTex);
     glDeleteProgram(screenShaderProgram);
     glDeleteProgram(ComputeShaderProgram);
+    ErrorLogger("Application Finished, Terminating");
     glfwTerminate();
     return EXIT_SUCCESS;
 }
